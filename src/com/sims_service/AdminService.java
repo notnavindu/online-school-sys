ad63e4a3369a744ed9fd38f8691bbacd83970059
Menake Dasunpriya <menakedasunpriya@gmail.com>
2021-05-01 23:44:17 +0530

    added AdminService.java

--- src/com/sims_service/AdminService.java
+++ src/com/sims_service/AdminService.java
@@ -1,15 +1,16 @@
 package com.sims_service;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import com.sims_models.Student;
+import com.sims_models.Teacher;
 import com.sims_util.DbConnection;
 
-public class AddStudentDao {
+
+public class AdminService {
 	private static Connection con;
 	
 	public static int addStudent(Student std) {
@@ -54,4 +55,46 @@ public class AddStudentDao {
 		
 	}
 	
+	public static void addTeacher(Teacher teach) {
+		try {
+			
+			con = DbConnection.getConnection();
+			
+			String query = "insert into online_school_ims.teacher values (?,?,?,?,?,?,?)";
+			
+			PreparedStatement stmt = con.prepareStatement(query);
+			
+			stmt.setInt(1, teach.getTid());
+			stmt.setString(2, teach.getName());
+			stmt.setInt(3, teach.getAge());
+			stmt.setString(4, teach.getAddress());
+			stmt.setString(5, teach.getContact());
+			stmt.setInt(6, teach.getSbid());
+			stmt.setString(7, teach.getProfilePic());
+			
+			int i = stmt.executeUpdate();
+			//System.out.print(i + "rows added");
+			System.out.print(stmt);
+			
+			/*
+			PrintWriter out;
+			
+			if(i>0) {
+				
+			}
+			*/
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
--- src/com/sims_servlets/AddStudent.java
+++ src/com/sims_servlets/AddStudent.java
@@ -1,9 +1,6 @@
 package com.sims_servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,11 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.sims_models.Student;
-import com.sims_service.AddStudentDao;
+import com.sims_service.AdminService;
 
-/**
- * Servlet implementation class AddStudent
- */
+
 @WebServlet("/AddStudent")
 public class AddStudent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -24,35 +19,25 @@ public class AddStudent extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		String name = request.getParameter("sname");
 		int age = Integer.parseInt(request.getParameter("age"));
 		String contact = request.getParameter("contact");
 		String address = request.getParameter("address");
 		int grade = Integer.parseInt(request.getParameter("grade"));
 		String className = request.getParameter("class");
-		String userName = 
+		String userName;
 		
 		//TODO :Figure out image uploading
 		String image = "random link";
-		
+				
 		Student std = new Student(0,name,age,address,contact, grade, className, image);
-		
-		int i = AddStudentDao.addStudent(std);
-	
-		
+				
+		int i = AdminService.addStudent(std);
+			
+				
 		//Redirect to Enroll.jsp
 		response.sendRedirect("enroll.jsp");
 		
-		//request.setAttribute("name", name);
-		//request.setAttribute("age", age);
-		//request.setAttribute("contact", contact);
-		//request.setAttribute("address", address);
-		//request.setAttribute("grade", grade);
-		//request.setAttribute("className", className);
-		
-		//RequestDispatcher rd = request.getRequestDispatcher("AddStudentDao");
-		//rd.forward(request, response);
 	}
 
 }
